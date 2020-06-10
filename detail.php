59ef5644282e6d017cf4c98f20d1015c2c685818
ferrariomatias <ferrariomatias@gmail.com>
2020-06-10 20:13:52 -0300

    creada la preferencia de pago

--- detail.php
+++ detail.php
@@ -124,12 +124,37 @@
                                             </h3>
                                         </div>
                                         <h3 >
-                                            <?php echo $_POST['price'] ?>
+                                            <?php echo "$" . $_POST['price'] ?>
                                         </h3>
                                         <h3 >
-                                            <?php echo "$" . $_POST['unit'] ?>
+                                            <?php echo $_POST['unit'] ?>
                                         </h3>
                                     </div>
+                                    <?php
+                                        // SDK de Mercado Pago
+                                        require __DIR__ .  '/vendor/autoload.php';
+
+                                        // Agrega credenciales
+                                        MercadoPago\SDK::setAccessToken('PROD_ACCESS_TOKEN');
+
+                                        // Crea un objeto de preferencia
+                                        $preference = new MercadoPago\Preference();
+
+                                        // Crea un ítem en la preferencia
+                                        $item = new MercadoPago\Item();
+                                        $item->title = $_POST['title'];
+                                        $item->quantity = $_POST['unit'];
+                                        $item->unit_price = $_POST['price'];
+                                        $preference->items = array($item);
+                                        $preference->save();
+                                    ?>
+
+                                    <form action="/procesar-pago" method="POST">
+                                    <script
+                                    src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+                                    data-preference-id="<?php echo $preference->id; ?>">
+                                    </script>
+                                    </form>
                                     <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
                                 </div>
                             </div>
