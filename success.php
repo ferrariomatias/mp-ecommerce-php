97945d0fb8438b6b8f4e4343c56a69aeb97d1391
ferrariomatias <ferrariomatias@gmail.com>
2020-06-11 11:24:21 -0300

    agrego payment type a succcess

--- success.php
+++ success.php
@@ -2,7 +2,7 @@
 
 var_dump(parse_url($url, PHP_URL_QUERY));
 
-echo 'payment_method_id: ' . $_GET['payment_method_id'];
+echo 'payment_type: ' . $_GET['payment_type'];
 echo '<br/>';
 echo 'external_reference: ' . $_GET['external_reference'];
 echo '<br/>';
