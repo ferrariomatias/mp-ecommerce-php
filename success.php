aa01aeb39f48736b41aa01325eb2771ae8b15a17
ferrariomatias <ferrariomatias@gmail.com>
2020-06-10 23:51:14 -0300

    muestro success correctamente

--- success.php
+++ success.php
@@ -1,3 +1,9 @@
 <?php
 
 var_dump(parse_url($url, PHP_URL_QUERY));
+
+echo 'payment_method_id: ' . $_GET['payment_method_id'];
+echo '<br/>';
+echo 'external_reference: ' . $_GET['external_reference'];
+echo '<br/>';
+echo 'collection_id: ' . $_GET['collection_id'];
