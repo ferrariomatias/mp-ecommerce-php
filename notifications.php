e62f8014dc6365e9be21ff0632577e3fea8c7694
ferrariomatias <ferrariomatias@gmail.com>
2020-06-11 12:16:50 -0300

    muestro notificacion

--- notifications.php
+++ notifications.php
@@ -1,7 +1,35 @@
 <?php
 require __DIR__ .  '/vendor/autoload.php';
 MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
-file_put_contents('php://stderr', print_r('hola', true));
+
+
+//Make sure that it is a POST request.
+if (strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0) {
+    throw new Exception('Request method must be POST!');
+}
+ 
+//Make sure that the content type of the POST request has been set to application/json
+$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+if (strcasecmp($contentType, 'application/json') != 0) {
+    throw new Exception('Content type must be: application/json');
+}
+ 
+//Receive the RAW post data.
+$content = trim(file_get_contents("php://input"));
+ 
+//Attempt to decode the incoming RAW post data from JSON.
+$decoded = json_decode($content, true);
+ 
+//If json_decode failed, the JSON is invalid.
+if (!is_array($decoded)) {
+    throw new Exception('Received content contained invalid JSON!');
+}
+ 
+//Process the JSON.
+file_put_contents('php://stderr', print_r('CONTENIDO', true));
+file_put_contents('php://stderr', print_r($content, true));
+file_put_contents('php://stderr', print_r('DECODED', true));
+file_put_contents('php://stderr', print_r($decoded, true));
 
 switch ($_POST["type"]) {
     case "payment":
