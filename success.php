f51f5f2bc2ae99dac392650fe906b8e46ac6d247
ferrariomatias <ferrariomatias@gmail.com>
2020-06-11 12:45:48 -0300

    modifico success

--- success.php
+++ success.php
@@ -1,7 +1,8 @@
 <?php
 
-var_dump(parse_url($url, PHP_URL_QUERY));
-
+echo "El pago fue exitoso.";
+echo '<br/>';
+echo '<br/>';
 echo 'payment_type: ' . $_GET['payment_type'];
 echo '<br/>';
 echo 'external_reference: ' . $_GET['external_reference'];
