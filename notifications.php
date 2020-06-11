5222de5e794602055699ecbb13fbe6969804b8d2
ferrariomatias <ferrariomatias@gmail.com>
2020-06-11 12:05:20 -0300

    mejoro notification

--- notifications.php
+++ notifications.php
@@ -1,12 +1,10 @@
 <?php
-
-use MercadoPago\Payment;
-
+require __DIR__ .  '/vendor/autoload.php';
 MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
 switch ($_POST["type"]) {
     case "payment":
-        $payment = Payment::find_by_id($_POST["id"]);
+        $payment = MercadoPago\Payment::find_by_id($_POST["id"]);
         break;
     case "plan":
         $plan = MercadoPago\Plan::find_by_id($_POST["id"]);
